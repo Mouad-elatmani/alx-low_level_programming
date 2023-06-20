0cd783f5fcf01392d4e47186ac0a50d1e57e7f05
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-06-20 09:30:36 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,8 @@
 */
 int main(void)
 {
-	int str[8] = {95, 113, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	int str[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count ;
 
 	for (count = 0; count < 8; count++)
 	{
