356cb37a372962aa047e1ccefee2ffec08aea1b2
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-06-20 09:54:55 -0700

    print alphabet 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Entry point
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	char x;
+	int num = 0;
+
+	while (i < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
