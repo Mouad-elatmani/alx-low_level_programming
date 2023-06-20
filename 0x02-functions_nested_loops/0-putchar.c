c81b0ab5bffc150435b176751d1aa35c115f8596
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-06-20 09:19:17 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,10 @@
 */
 int main(void)
 {
-	int str[] = {95, 113, 117, 116, 99, 104, 97, 114};
+	int str[8] = {95, 113, 117, 116, 99, 104, 97, 114};
 	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	for (count = 0; count < 8; count++)
 	{
 		_putchar(str[count]);
 	}
