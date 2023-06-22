19baea811fb0764e46cd4eea615c62ec6c5b5b00
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-06-22 08:52:09 -0700

    checks upercase character

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
