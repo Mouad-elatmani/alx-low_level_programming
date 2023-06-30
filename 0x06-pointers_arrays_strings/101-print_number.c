72599cbeeb9c23893f789b02d915eeb0361de6fb
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-06-30 14:13:31 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,29 +6,29 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int val, div, c;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		val = n * -1;
 	}
 	else
 	{
-		m = n;
+		val = n;
 	}
 
-	d = m;
-	count = 1;
+	div = val;
+	c = 1;
 
-	while (d > 9)
+	while (div > 9)
 	{
-		d /= 10;
-		count *= 10;
+		div /= 10;
+		c *= 10;
 	}
 
-	for (; count >= 1; count /= 10)
+	for (; c >= 1; c /= 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+		_putchar(((val / c) % 10) + 48);
 	}
 }
