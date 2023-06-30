123fef7f2bac01955b82c7667d31b01c7de2f4e2
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-06-30 13:40:11 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	int temp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	else
+		temp = n;
+
+	if (temp < 10)
+		_putchar(temp + '0');
+	else
+	{
+		_putchar((temp / 10) + '0');
+		_putchar((temp % 10) + '0');
+	}
+}
