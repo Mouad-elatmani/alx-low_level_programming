b226ddf8afcfc1e68c171e1781f7bb72d68c40d9
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-07-11 16:11:53 -0700

    allooc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	tab = malloc(sizeof(int) * height);
+	tab = malloc(sizeof(int *) * height);
 
 	if (tab == NULL)
 		return (NULL);
