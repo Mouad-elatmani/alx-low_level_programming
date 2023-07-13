8513f9a7b532c755b44a9422f1bf445fd5965ccd
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-07-13 07:04:01 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int ls1, ls2, lsout, i;
+	unsigned int ls1, ls2, res, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,23 +21,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++);
 
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++);
 
 	if (n > ls2)
 		n = ls2;
 
-	lsout = ls1 + n;
+	res = ls1 + n;
 
-	p = malloc(lsout + 1);
+	p = malloc(res + 1);
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < lsout; i++)
+	for (i = 0; i < res; i++)
 		if (i < ls1)
 			p[i] = s1[i];
 		else
