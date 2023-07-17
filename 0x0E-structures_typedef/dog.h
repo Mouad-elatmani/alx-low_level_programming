ded99b4eb2869461da30f721df20cab197203d0e
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-07-17 05:59:09 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ * @name: First champ
+ * @age: Second champ
+ * @owner: Third champ
+ *
+ * Description: Dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+	
+
+
+
+
+
+
+
+
+#endif
