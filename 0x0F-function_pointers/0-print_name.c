6daa6300d8fff0b3ba5a155253766b1b0a8b516a
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-07-19 14:31:05 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
-
 /**
  * print_name - print aname
  * @name: the name what want to printed
  * @f: pointer
  *
- *  * Rteurn: void
  */
+
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
+
 		f(name);
-	else
-		return;
 }
