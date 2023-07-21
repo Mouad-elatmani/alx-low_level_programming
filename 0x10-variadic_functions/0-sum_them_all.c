a85b019a8d47984f42d23304380a25d7a5b2c346
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-07-20 17:25:39 -0700

    variadic _functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,8 +16,8 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(list, int);
 
-	va_end(ap);
+	va_end(list);
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,10 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
+		printf("%d", va_arg(list, int));
 		if (i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
-	va_end(ap);
+	va_end(list);
 }
