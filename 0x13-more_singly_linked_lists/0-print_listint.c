70a41a46917b6543a644a703e6b1425493f688bc
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-07-31 14:47:25 -0700

    print list integer

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints the lements of lists
+ * @h: listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
