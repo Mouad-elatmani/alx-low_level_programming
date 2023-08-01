9d451b7fa491d9441a8baa4202722e003231dd16
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-08-01 15:34:43 -0700

    inser a node in a specific index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,12 +25,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	for (i = 0; i < idx - 1 && temp; i++)
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
 	{
 		temp = temp->next;
 	}
 	if (temp == NULL && idx != 0)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->next = temp->next;
 	temp->next = new;
