a64a2c93b87426f6b7b3cded07e757c5d7a44614
Mouad El Atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-08-01 14:24:39 -0700

    pop linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node 
+ * @head: pointer to the first node  in the listint_t
+ *
+ * Return: returns the head node’s data (n), or 0 if the listint_t is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (*head == NULL)
+		return (0);
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}
