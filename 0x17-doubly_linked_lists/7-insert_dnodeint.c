d22998e195b7477ee49758837879c7edafa7feee
mouad el atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-09-14 13:29:55 -0700

    insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,37 +7,39 @@
  * @n: value of the new node
  * Return: address of the new node, or NULL if it fails
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	unsigned int cnt = 0;
-	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
 	if (h == NULL)
 		return (NULL);
-	if (new == NULL)
+
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+
 	while (temp)
 	{
 		if (cnt == (idx - 1))
 		{
-			new->prev = temp;
-			new->next = temp->next;
+			new_node->prev = temp;
+			new_node->next = temp->next;
 			if (temp->next != NULL)
-				temp->next->prev = new;
-			temp->next = new;
-			return (NULL);
+				temp->next->prev = new_node;
+			temp->next = new_node;
+			return (new_node);
 		}
 		cnt++;
 		temp = temp->next;
 	}
-	free(new);
+	free(new_node);
 	return (NULL);
 }
